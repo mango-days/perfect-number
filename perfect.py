289f9773a0c063380770df4d21e1dd502e6a4193
mango-days <77330707+mango-days@users.noreply.github.com>
2021-10-28 20:53:02 +0530

    Update perfect.py

--- perfect.py
+++ perfect.py
@@ -1,19 +1,12 @@
-def power(number, pow):
-  ans = 1
-  while pow!=1:
-    ans = ans*number
-    pow -=1
-  return ans
-
 number = input()
-pow = 1
+p = 1
 flag = 0
 while flag == 0 :
-  if number == (power(2, pow)*(power(2, pow+1)-1)):
+  if number == (pow(2, p)*(pow(2, p+1)-1)):
     print ("perfect number")
     flag = 1
-  if number < (power(2, pow)*(power(2, pow+1)-1)):
+  if number < (pow(2, p)*(pow(2, p+1)-1)):
     print("not a perfect number")
     flag = 1
-  if number > (power(2, pow)*(power(2, pow+1)-1)):
-    pow+=1
+  if number > (pow(2, p)*(pow(2, p+1)-1)):
+    p+=1
